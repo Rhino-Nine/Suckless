b55eb991638d4ee131dc531c10451c82a5c036f7
Rhino-Nine <110172475+Rhino-Nine@users.noreply.github.com>
2023-09-30 20:29:37 +0800

    refactor(dwm): rm needless startup stuff

--- dwm/scripts/run.sh
+++ dwm/scripts/run.sh
@@ -6,17 +6,13 @@ xset r rate 350 50 &
 xset mouse 1.3 &
 xrander -s 1920x1080 &
 
-# /home/rhino/.local/bin/cron/cleaner &
 pulseaudio &
 clash &
 fcitx5 -D &
 picom &
 mpd &
-# syncthing &
 dunst &
 brillo -S 100 &
 pamixer -u --set-volume 30 &
-# eww & 
-# play-with-mpv &
 
 while type dwm >/dev/null; do dwm && continue || break; done
